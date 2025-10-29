98f4b1a9d888a11a6f7b8188705215d8227ddd76
Alesandra Júlia <alessandrajulia0208@gmail.com>
2025-10-28 23:07:26 -0300

    veasy tela 1

--- veasy/app/index.tsx
+++ veasy/app/index.tsx
@@ -1,15 +1,56 @@
-import { Text, View } from "react-native";
+import React , {useState} from "react";
+import { Text, View, ImageBackground, TextInput} from "react-native";
+import { Stack, withLayoutContext } from "expo-router";
 
-export default function Index() {
+export default function App() {
+  const [emailfield, setEmailField] = useState('')
+  const [email, setEmail] = useState('')
+
+  const [senhafield, setSenhaField] = useState('')
+  const [senha, setSenha] = useState('')
+
   return (
-    <View
+    <ImageBackground
+      source={require("../assets/images/fundo.jpg")}
       style={{
-        flex: 1,
+        flex: 1, // 🔹 ocupa toda a tela
+        width: "100%",
+        height: "100%",
         justifyContent: "center",
         alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+        
+        }}>
+      <View>
+        <Text style ={{
+          color: "#fff",
+            fontSize: 45,
+            top:-150,
+            fontWeight: "bold",
+        }}>
+          Veasy
+        </Text>
+      </View>
+
+      <View
+        style={{
+          backgroundColor: "rgba(0,0,0,0.3)", // leve transparência
+          padding: 100,
+          borderRadius: 10,
+          borderColor: "rgba(255, 255, 255, 1)",
+          borderWidth: 2,
+
+        }}>
+
+        <Text
+          style={{
+            color: "#fff",
+            fontSize: 20,
+            textAlign: "center",
+            }}>
+
+          Entrar{"\n"}
+        </Text>
+      </View>
+    </ImageBackground>
   );
 }
