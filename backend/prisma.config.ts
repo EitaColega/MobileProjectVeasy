930a377cadebdc628a0eef0ef56e8f12f09da74e
fumiko0701 <zx3rainbow@gmail.com>
2025-11-21 17:19:45 -0300

    Funcionalidades de email finalizadas

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,10 +1,13 @@
+//backend/prisma.config.ts
 import dotenv from "dotenv";
 dotenv.config();
 import { defineConfig, env } from "prisma/config";
 
+import path from "path";
+
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: path.resolve(__dirname, "prisma", "schema.prisma"),
   migrations: {
     path: "prisma/migrations",
   },
